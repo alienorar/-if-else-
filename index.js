92496016a4b6d67b226b85c94893942e06db6c86
alienorar <rayhonakhayrullaeva@gmail.com>
2024-06-28 17:36:21 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// let number = +prompt("son kirit")
+// let random_number = (parseInt(Math.random) * 10)
+
+// console.log(random_number);
+// console.log(number);
+
+// const difference=() =>{
+//     result=""
+//   if (number > random_number) {
+//       result = console.log("son randomdan katta");
+//   }else{
+//     result=  console.log("son randomdan kichik");
+//   }
+
+//   }
+
+// difference()
+
+
+
+
+
+
+
+
+let number = +prompt("son kirit")
+
+const countNumber = function () {
+    if (!isNaN(number)) {
+console.log(number);
+    }else{
+        console.log("bu son emas");
+    }
+}
+countNumber()
+
+   
+
+
+
+
+
+
+
+
+
+//         if (number % 2 == 0) {
+//             result = `${number}  juft son`
+          
+//         }else{
+//             result = `${number}  toq son`
+            
+//         }
+
+//     } else{
+//         result = `string kiritildi`
+       
+//     }
+//     return number ? result : "qiymat kirit baraka topgur";
+// }
+// totalCount()
