6080c31afdfc97a0c827697daf26a9dcbfe19304
alienorar <rayhonakhayrullaeva@gmail.com>
2024-06-29 21:29:57 +0500

    first commit

---
+++ classwork/index.js
@@ -0,0 +1,140 @@
+// let number = +prompt("son kirit")
+// let random_number = (parseInt(Math.random) * 10)
+
+// console.log(random_number);
+// console.log(number);
+
+// const difference=() =>{
+//     result=""
+//   if (number > random_number) {
+//       result = console.log("son randomdan katta");
+//   }else{
+//     result=  console.log("son randomdan kichik");
+//   }
+
+//   }
+
+// difference()
+
+
+
+
+
+
+
+
+// let number = +prompt("son kirit")
+
+// const countNumber = function () {
+//     if (!isNaN(number)) {
+// console.log(number);
+//     }else{
+//         console.log("bu son emas");
+//     }
+// }
+// countNumber()
+
+
+
+//         if (number % 2 == 0) {
+//             result = `${number}  juft son`
+
+//         }else{
+//             result = `${number}  toq son`
+
+//         }
+
+//     } else{
+//         result = `string kiritildi`
+
+//     }
+//     return number ? result : "qiymat kirit baraka topgur";
+// }
+// totalCount()
+
+
+
+// let a = 6
+
+// let b = 7
+
+// let c = 8
+
+// function minValue(a,b,c) {
+//     if (a > b && a > c) {
+//         return c ;
+//     }else if (b > a && b > c) {
+//         return b;
+//     } else if (c > b && c > a) {
+//         return a;
+//     } }
+// console.log(minValue(4,5,6));
+
+
+// 1.masala
+
+// function numdegree(number,degree) {
+// return number ** degree
+
+// }
+// console.log(numdegree(4,3));
+
+// 2.masala
+
+// function remain(a,b) {
+//     if (a > b) {
+//         return a % b;
+//     }
+//    return b % a;
+// }
+// console.log(remain(3,7));
+
+
+// let number = 12;
+
+// function checkFizzBazz(number) {
+//     if (number % 5 == 0 && number % 3 == 0) {
+//        console.log("fizzbazz");
+//     } else if (number % 5 == 0) {
+//         console.log("fizz");
+//     } else if (number % 3 == 0){
+//         console.log("bazz");
+//     }
+//     return number;
+// }
+//  console.log(checkFizzBazz(10));
+
+
+
+// const getroom =(number) =>{
+//     if (number >= 1000) {
+//         console.log("lux xona");
+//     } else {
+//         if (number >= 500) {
+//             console.log("yarim lux xona")
+//         }
+//         else {
+//             if (number >= 200) {
+//                 console.log("oddiy xona")
+//             } else {
+//                 console.log("xona yuq")
+//             }
+//         }
+//     }
+// return number
+// }
+// console.log(getroom(700));
+
+// function getGradeResult(grade){
+//     if(grade == 5){
+//         console.log("A'lo baxo")
+//     } else if (grade == 4){
+//         console.log("Yaxshi")
+//     } else if (grade == 3){
+//         console.log("Qoniqarsiz")
+//     } else{
+//         console.log("Bunaqa baxo mavjut emas")
+//     }
+//     return grade
+// }
+// console.log(getGradeResult(5))
--- index.js
+++ index.js
@@ -1,61 +1,109 @@
-// let number = +prompt("son kirit")
-// let random_number = (parseInt(Math.random) * 10)
 
-// console.log(random_number);
+
+// extra task 1
+
+// function sortDigits() {
+//     let number = +prompt("Enter a three-digit number:");
+
+//     console.log(number);
+
+//     let a = parseInt(number / 100);
+//     let b = parseInt(((number % 100) - (number % 10)) / 10);
+//     let c = parseInt(number % 10);
+
+//     if (a > b && b > c) {
+//         console.log(`${c} ${b} ${a}`);
+//     } else if (a > c && c > b) {
+//         console.log(`${b} ${c} ${a}`);
+//     } else if (b > a && a > c) {
+//         console.log(`${c} ${a} ${b}`);
+//     } else if (b > c && c > a) {
+//         console.log(`${a} ${c} ${b}`);
+//     } else if (c > a && a > b) {
+//         console.log(`${b} ${a} ${c}`);
+//     } else if (c > b && b > a) {
+//         console.log(`${a} ${b} ${c}`);
+//     }
+
+// }
+
+// sortDigits();
+
+
+
+
+// task 1
+// let number1 = +prompt
+// let number2 = +prompt
+
+// let result1 = number1 + number2
+// console.log(result1);
+
+// let result2 = number1 - number2
+// console.log(result2);
+
+// let result3 = number1 * number2
+// console.log(result3);
+
+// let result4 = number1 / number2
+// console.log(result4);
+
+
+// task 2
+// let number = +prompt("enter number")
 // console.log(number);
 
-// const difference=() =>{
-//     result=""
-//   if (number > random_number) {
-//       result = console.log("son randomdan katta");
-//   }else{
-//     result=  console.log("son randomdan kichik");
-//   }
-
-//   }
-
-// difference()
-
-
-
-
-
-
-
-
-let number = +prompt("son kirit")
-
-const countNumber = function () {
-    if (!isNaN(number)) {
-console.log(number);
-    }else{
-        console.log("bu son emas");
-    }
-}
-countNumber()
-
-   
-
-
-
-
-
-
-
-
-
-//         if (number % 2 == 0) {
-//             result = `${number}  juft son`
-          
-//         }else{
-//             result = `${number}  toq son`
-            
-//         }
-
-//     } else{
-//         result = `string kiritildi`
-       
-//     }
-//     return number ? result : "qiymat kirit baraka topgur";
+// if (number > 0) {
+//     console.log(number - 1);
+// }else{
+//     console.log(number + 1);
 // }
-// totalCount()
+
+
+// task 3
+// const reverse = (number) => {
+//     if (number > 9 && number < 100) {
+//         let a = parseInt(number / 10)
+//         console.log(a);
+
+//         let b = parseInt(number % 10)
+//         console.log(b);
+
+//     console.log(`${b} ${a}`);
+//     }else{
+//         console.log("You must enter only double-digits number");
+//     }
+// } 
+// reverse(23)
+
+// task 4
+// const reverse = (number) => {
+//     if (number >= 100 && number < 1000) {
+//         let a = parseInt(number / 100);
+//         console.log(a);
+//         let b = parseInt(((number % 100) - (number % 10)) / 10);
+//         console.log(b);
+//         let c = parseInt(number % 10);
+//         console.log(c);
+
+//         console.log(`${c}  ${b} ${a}`);
+//     } else {
+//         console.log("You must enter only three-digits number");
+//     }
+// }
+// reverse(321)
+
+
+// task 5
+// function checkGrade(number) {
+//     if (number <= 100 && number >= 70 ) {
+//         console.log(" You have got fully funded scolarship");
+//     }else if (number < 70 && number >= 50 ) {
+//         console.log(" You have accepted with tuition fee");
+//     } else  {
+//         console.log("Unfortunately , your score didn't fit for our university");
+//     }
+
+// }
+// checkGrade(65)
+
